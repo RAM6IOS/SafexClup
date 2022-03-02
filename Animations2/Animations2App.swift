d1399ae54feb4ce4e3e549691fd1aef244dbf43c
RAM6IOS <ios191169@gmail.comm>
2022-03-02 20:36:03 +0100

    Initial Commit

---
+++ Animations2/Animations2App.swift
@@ -0,0 +1,17 @@
+//
+//  Animations2App.swift
+//  Animations2
+//
+//  Created by Bouchedoub Rmazi on 2/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct Animations2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations2
+//
+//  Created by Bouchedoub Rmazi on 2/3/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
